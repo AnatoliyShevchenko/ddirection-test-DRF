2bf15d62bc4d79890ca22de23c8f4518f84fe6df
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-06-05 10:04:48 +0500

    finish

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 from decouple import config
 
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -108,6 +110,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -22,3 +22,60 @@ class TaskSerializer(serializers.ModelSerializer):
             obj.due_date < timezone.now().date()
             and obj.status != Status.DONE
         )
+
+    def validate_title(self, value):
+        user = self.context["request"].user
+        if Task.objects.filter(title=value, user=user).exclude(
+            status=Status.DONE).exists():
+            raise serializers.ValidationError(
+                "You're already have incomplete task with this title!"
+            )
+        return value
+
+    def validate_status(self, value):
+        if self.instance is None and value != Status.NEW:
+            raise serializers.ValidationError(
+                "You should create task with status 'new'"
+            )
+        return value
+
+    def validate_due_date(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError(
+                "Due date cannot be in the past."
+            )
+        return value
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        return Task.objects.create(user=user, **validated_data)
+
+class TaskQuerySerializer(serializers.Serializer):
+    """Serializer for validation filters."""
+
+    title = serializers.CharField(
+        max_length=50, required=False
+    )
+    status = serializers.ChoiceField(
+        choices=Status.choices, required=False
+    )
+    order = serializers.ChoiceField(
+        choices=["asc", "desc"], required=False
+    )
+    sortBy = serializers.ChoiceField(
+        choices=["due_date"], required=False
+    )
+
+    def validate(self, attrs):
+        order = attrs.get("order")
+        sort_by = attrs.get("sortBy")
+
+        if order and not sort_by:
+            raise serializers.ValidationError({
+                "sortBy": "This field is required when 'order' is provided."
+            })
+        if sort_by and not order:
+            raise serializers.ValidationError({
+                "order": "This field is required when 'sortBy' is provided."
+            })
+        return attrs
